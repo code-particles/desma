f1602b3167e11e169b9e7bb0eec5820fc4a20d1d
Henry Ohanga <ohanga.henry@gmail.com>
2020-06-25 23:44:32 +0300

    restructure app setup

--- src/Sms.php
+++ src/Sms.php
@@ -5,7 +5,7 @@
  * @license https://desma.particles.co.ke/license/
  */
 
-namespace particles\Desma;
+namespace particles\desma;
 
 const SMS_URL = "https://2l4np.api.infobip.com/sms/2/text/advanced";
 
